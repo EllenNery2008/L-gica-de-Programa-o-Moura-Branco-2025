86aff2eec176aa7e9e526a90442db2a4a83cdfeb
Ellen <ellennery9@gmail.com>
2025-05-28 09:25:45 -0300

    Desafios de programacao

---
+++ Exercicios de programacao/desafios/desafios_11.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+  
+    for(int contador = 5; contador >= 1;contador--){ //
+      Serial.println(contador); 
+  }
+}
+
+void loop()
+{  
+  //sem instrucoes
+}
